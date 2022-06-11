1032ae4e59fb18793ab0169656fdc0d722c59fa9
rldnrl <devpark1002@gmail.com>
2022-06-11 20:41:50 +0900

    feat: implement auth handler

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,23 @@
+import { NextApiHandler } from 'next'
+import NextAuth from 'next-auth'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import GitHubProvider from 'next-auth/providers/github'
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const options = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  ],
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.SECRET,
+}
+
+const authHandler: NextApiHandler = (req, res) => NextAuth(req, res, options)
+
+export default authHandler
